71c0910c42f4b3435396c41c4fce1ad3519cb6cc
francisco vagner vasconcelos <106701917+francisco-vagner@users.noreply.github.com>
2022-07-30 00:28:15 -0300

    Add files via upload

---
+++ desafio2.js
@@ -0,0 +1,17 @@
+"use strict";
+let pessoa1;
+let pessoa2 = {
+    nome: "roberto",
+    idade: 19,
+    profissao: "Padeiro"
+};
+let pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+let pessoa4 = {
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
+};
