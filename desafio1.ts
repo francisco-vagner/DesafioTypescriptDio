366cb020abe91e68b0a97b93afc91757a70014bf
francisco vagner vasconcelos <106701917+francisco-vagner@users.noreply.github.com>
2022-07-30 00:03:34 -0300

    Add files via upload

---
+++ desafio1.ts
@@ -0,0 +1,21 @@
+let funcionario1 = {
+    codigo : 10,
+    nome : 'francisco josé da silva',
+    endereco: "Rua das flores",
+    numeroCasa : 15,
+    filiacao: "maria e joao"
+    };
+    
+    let funcionario2 = 
+    {
+        codigo : 11,
+        nome : 'francisco',
+        endereco: "Rua do rosário",
+        numeroCasa: 14,
+        filiacao: "josepha e pedro"
+    }
+    
+    console.log("Funcionário -              Endereço -         numero da casa -        filiacao");
+    console.log(funcionario1.nome +"    " +funcionario1.endereco +"     "+funcionario1.numeroCasa+"                      "+funcionario1.filiacao);
+    console.log(funcionario2.nome +"                  "+ funcionario2.endereco + "     "+funcionario2.numeroCasa+"                     "+funcionario2.filiacao);
+    
